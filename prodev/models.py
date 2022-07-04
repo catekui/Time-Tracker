6a75af79a80322c9bc0970e7c8eb4dbaae6efdc5
catekui <catherine.wanjiku@student.moringaschool.com>
2022-07-04 23:24:57 +0300

    added models

--- prodev/models.py
+++ prodev/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=250, unique=True)
+    password = models.CharField(max_length=255)
+    profession = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='cloudinary')
+    
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    description = models.CharField(max_length=255)
+    time_interval = models.IntegerField(default=None)
+    break_interval = models.IntegerField(default=None)
+    activity = models.CharField(max_length=255)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+class Reviews(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    efficiency = models.IntegerField()
+    experience = models.IntegerField()
+    productivity = models.IntegerField()
+
+
+
+
+    
+
